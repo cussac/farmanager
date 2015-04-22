2f5dcff589125762cf45ab55aaba3c60e35e5d7a
cussac <alvarocussac@hotmail.es>
2015-04-22 17:24:34 +0200

    pdf

--- src/DWES/farmanagerBundle/Controller/PdfController.php
+++ src/DWES/farmanagerBundle/Controller/PdfController.php
@@ -5,14 +5,30 @@ namespace DWES\farmanagerBundle\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use DWES\farmanagerBundle\Entity\Comentario;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\BinaryFileResponse;
 
 
 class PdfController extends Controller
 {
-    public function pdfAction()
+    public function pdfAction($id)
     {
-        $html = $this->renderView('DWESfarmanagerBundle:Default:pdf.html.twig'
+        $em = $this->getDoctrine()->getManager();
+
+        $entrada = $em->getRepository('DWESfarmanagerBundle:Entrada')->find($id);
+
+        if(!$entrada)
+        {
+            throw $this->createNotFoundException(
+                'No existe nigúna entrada con id '.$id);
+        }
+
+        $params = array(
+            'entrada' => $entrada,
         );
+
+        $html = $this->renderView('DWESfarmanagerBundle:Default:pdf.html.twig'
+                ,$params);
 
         return new Response(
             $this->get('knp_snappy.pdf')->getOutputFromHtml($html),
